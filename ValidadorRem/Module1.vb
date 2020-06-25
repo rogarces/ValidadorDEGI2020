3ffe8115ce68cf2ccf15bde24af3658d42f10703
Rodrigo Garces <rodrigo.garces@hotmail.cl>
2020-06-25 15:46:51 -0400

    25.06.2020

---
+++ ValidadorRem/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+    Public Function nreg(Hoja As Microsoft.Office.Interop.Excel.Worksheet, nFila As Long, nColumna As Long)
+
+        Do Until Hoja.Cells(nFila, nColumna).value = ""
+            nFila = nFila + 1
+        Loop
+        Return nFila
+    End Function
+End Module
